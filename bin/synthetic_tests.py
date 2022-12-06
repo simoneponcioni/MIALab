635c5858552017b7618087c922f2a3b1c5658f15
Simone <simone.poncioni@gmail.com>
2022-12-06 16:43:13 +0100

    change RF params

--- bin/synthetic_tests.py
+++ bin/synthetic_tests.py
@@ -6,13 +6,13 @@ from pathlib import Path
 
 def create_circular_mask(h, w, center=None, radius=None):
 
-    if center is None: # use the middle of the image
-        center = (int(w/2), int(h/2))
-    if radius is None: # use the smallest distance between the center and image walls
-        radius = min(center[0], center[1], w-center[0], h-center[1])
+    if center is None:  # use the middle of the image
+        center = (int(w / 2), int(h / 2))
+    if radius is None:  # use the smallest distance between the center and image walls
+        radius = min(center[0], center[1], w - center[0], h - center[1])
 
     Y, X = np.ogrid[:h, :w]
-    dist_from_center = np.sqrt((X - center[0])**2 + (Y-center[1])**2)
+    dist_from_center = np.sqrt((X - center[0]) ** 2 + (Y - center[1]) ** 2)
 
     mask = dist_from_center <= radius
     return mask
@@ -41,14 +41,13 @@ def create_cv2_circle(x_a, y_a, x_b, y_b, radius_a, radius_b, i, show_plot=True)
     circle_a[mask_a] = 1
     circle_b[mask_b] = 1
 
-
     if show_plot is not False:
         plot_combined = circle_a + circle_b
         plt.imshow(plot_combined, cmap="gist_gray", vmin=0, vmax=2)
-        plt.title(f'Offset circle = {dist_c:.1f} px', weight="bold")
+        plt.title(f"Offset circle = {dist_c:.1f} px", weight="bold")
         plt.colorbar()
-        savepath = r'sbd_results'
-        filepath = Path(savepath, f'circle_{i}.png')
+        savepath = r"sbd_results"
+        filepath = Path(savepath, f"circle_{i}.png")
         plt.savefig(filepath)
         plt.show()
     return circle_a, circle_b
@@ -58,7 +57,9 @@ def test_circles(i, c1_a, c2_a, c1_b, c2_b, radius_a, radius_b):
     """
     Test if the circle is created correctly.
     """
-    arr_a, arr_b = create_cv2_circle(c1_a, c2_a, c1_b, c2_b, radius_a, radius_b, i, show_plot=True)
+    arr_a, arr_b = create_cv2_circle(
+        c1_a, c2_a, c1_b, c2_b, radius_a, radius_b, i, show_plot=True
+    )
     metric = sbd.SBD_metric()
     sbd_sym = metric.Symmetric_Boundary_Dice(arr_a, arr_b)
     return sbd_sym
@@ -66,8 +67,9 @@ def test_circles(i, c1_a, c2_a, c1_b, c2_b, radius_a, radius_b):
 
 def plot_evolution(dx, sbd_arr):
     plt.figure(figsize=(10, 10))
-    plt.plot(dx, sbd_arr, color='tab:grey', linewidth=2)
+    plt.plot(dx, sbd_arr, color="tab:grey", linewidth=2)
     plt.show()
+
 
 def circles_dxdy(dx, radius_a, c1_a, c2_a):
     sbd_arr = []
@@ -79,6 +81,7 @@ def circles_dxdy(dx, radius_a, c1_a, c2_a):
         sbd_sym = test_circles(i, c1_a, c2_a, c1_b, c2_b, radius_a, radius_b)
         sbd_arr.append(sbd_sym)
     return sbd_arr
+
 
 def circles_dr(dr, radius_a, c1_a, c2_a):
     sbd_arr = []
@@ -92,8 +95,9 @@ def circles_dr(dr, radius_a, c1_a, c2_a):
         sbd_arr.append(sbd_sym)
     return sbd_arr
 
+
 if __name__ == "__main__":
-    # code for running evaluation of SBD metric with increasing distance
+    # code for running evaluation of SBD metric with increasing distance
     dx = np.linspace(0, 150, 5)
     radius_a = 100
     c1_a = 500
@@ -101,7 +105,7 @@ if __name__ == "__main__":
     sbd_arr = circles_dxdy(dx, radius_a, c1_a, c2_a)
     plot_evolution(dx, sbd_arr)
 
-    # code for running evaluation of SBD metric with decreasing radius
+    # code for running evaluation of SBD metric with decreasing radius
     dr = np.linspace(0, 50, 5)
     print(dr)
     radius_a = 100
