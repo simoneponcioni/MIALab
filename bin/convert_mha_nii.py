8c5be90470313c47e016c6414d44a9d08ee56ddc
Simone <simone.poncioni@gmail.com>
2022-12-11 16:41:07 +0100

    Created automated masking of physical groups

--- bin/convert_mha_nii.py
+++ bin/convert_mha_nii.py
@@ -1,6 +1,7 @@
 # convert mha to nii using SimpleITK
 import SimpleITK as sitk
 from pathlib import Path
+import numpy as np
 import glob
 
 
@@ -16,12 +17,34 @@ def convert_mha_nii(res_dir):
         sitk.WriteImage(sitk.ReadImage(str(i)), str(res_dir_nii / f"{i_stem}.nii.gz"))
 
 
+def create_mha_masks(res_dir):
+    masks_dir = Path(res_dir) / "masks"
+    masks_dir.mkdir(exist_ok=True)
+    result = glob.glob(f"{res_dir}/*.mha")
+
+    for img in result:
+        print(img)
+        img_sitk = sitk.ReadImage(img)
+        img_np = sitk.GetArrayFromImage(img_sitk)
+        img = Path(img)
+        img_stem = img.stem
+        Path(img.parent / "masks" / img_stem).mkdir(exist_ok=True)
+        img_stem = Path(Path(img).stem)
+
+        for m in range(1, 5):
+            mask = np.zeros(img_np.shape)
+            mask[img_np == m] = 1
+            mask_name = f"{img_stem}_mask_{m}.mha"
+            mask_path = img.parent / "masks" / img_stem / mask_name
+            sitk.WriteImage(sitk.GetImageFromArray(mask), str(mask_path))
+
+
 res_dir = "bin/mia-result/2022-12-06-16-50-06"
-# res_dir = "bin/mia-result/2022-11-06-16-12-39"
 
 
 def main():
-    convert_mha_nii(res_dir)
+    # convert_mha_nii(res_dir)
+    create_mha_masks(res_dir)
 
 
 if __name__ == "__main__":
